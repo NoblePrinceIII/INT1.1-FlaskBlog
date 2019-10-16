9db6ba19de425ae329b65d996f3a2b17b3266084
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-15 22:16:06 -0700

    code

--- flaskblog.py
+++ flaskblog.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, url_for
-
+from forms import RegistrationForm, LoginForm
 app = Flask(__name__)
+
+app.config["SECRET_KEY"] = "7bcc06e975497340fefd717dc88ef838"
 
 posts = [
     {
@@ -30,10 +32,16 @@ def about():
     return render_template("about.html", title="About")
 
 
+@app.route('/register')
+def register():
+    form = RegistrationForm()
+    return render_template("register.html", title="Register", form=form)
 
 
-
-
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template("login.html", title="Login", form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
