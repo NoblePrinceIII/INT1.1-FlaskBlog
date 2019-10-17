d5cdb6ef0e4c9df5c8deb2387b216013198fe5da
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-17 15:18:03 -0700

    Database:

--- forms.py
+++ forms.py
@@ -4,20 +4,19 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 
 class RegistrationForm(FlaskForm):
-    username = StringField("Username",
-    validators = [DataRequired(), Length(min=2, max=20)])
-    email = StringField("Email",
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
-    confirm_password = PasswordField("Confirm Password",
-                                    validators=[DataRequired(), EqualTo("password")])
-    submit = SubmitField("Sign Up")
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 
 class LoginForm(FlaskForm):
-    email = StringField("Email",
+    email = StringField('Email',
                         validators=[DataRequired(), Email()])
-    password = PasswordField("Password",
-                            validators=[DataRequired()])
-    remember = BooleanField("Remember Me")
-    submit = SubmitField("Login")
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
