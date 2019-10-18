2d1e7004afd7d43893899cd389427bc7eaf014bd
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-18 11:32:37 -0700

    comments

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -8,7 +8,7 @@ from flask_login import UserMixin
 def load_user(user_id):
     return User.query.get(int(user_id))
 
-
+# takes users inpute and enters it in database
 class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(20), unique=True, nullable=False)
@@ -16,12 +16,12 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', backref='author', lazy=True)
-
+# returns databse 
     def get_reset_token(self, expires_sec=1800):
         s = Serializer(app.config['SECRET_KEY'], expires_sec)
         return s.dumps({'user_id': self.id}).decode('utf-8')
 
-    @staticmethod
+    @staticmethod # so it tells python to read it as static
     def verify_reset_token(token):
         s = Serializer(app.config['SECRET_KEY'])
         try:
@@ -33,13 +33,13 @@ class User(db.Model, UserMixin):
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
-
+# class that takes user input and stores in databas
 class Post(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
+# returns database 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
