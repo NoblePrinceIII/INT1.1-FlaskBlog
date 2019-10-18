75c90c1ff2604735478572dee2483365b7e8dd3a
nobleprinceiii <farhan.begg@students.makeschool.com>
2019-10-17 21:42:40 -0700

    post.html

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -104,3 +104,10 @@ def new_post():
         return redirect(url_for('home'))
     return render_template('create_post.html', title='New Post',
                            form=form, legend='New Post')
+
+
+@app.route("/post/<int:post_id>")
+def post(post_id):
+    # Give Post with ID if doesn't exist return 404
+    post = Post.query.get_or_404(post_id)
+    return render_template('post.html', title=post.title, post=post)
